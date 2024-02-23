d47901cba5e0b0ea41f053391996705399f86f03
Jesse <wjesse12@gmail.com>
2024-02-23 12:56:57 -0500

    Added a new folder for Cipher Solvers

---
+++ CipherSolvers/VigenereCipher.js
@@ -0,0 +1,45 @@
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+function DecryptVigenere(cipher, key){
+    let cipherArray = cipher.split("");
+    let keyArray = key.split("");
+    let resultArray = [];
+
+    let currentKeyIndex = 0;
+    for(let i = 0; i < cipherArray.length; i++){
+        if(currentKeyIndex == keyArray.length){
+            currentKeyIndex = 0;
+        }
+        let currentLetterIndex = letters.indexOf(cipherArray[i]);
+        let keyIndex = letters.indexOf(keyArray[currentKeyIndex]);
+        let newLetterIndex = (currentLetterIndex + letters.length - keyIndex) % letters.length;
+        resultArray.push(letters[newLetterIndex]);
+        currentKeyIndex++;
+    }
+
+    return resultArray;
+}
+
+function EncryptVigenere(text, key){
+    let textArray = text.split("");
+    let keyArray = key.split("");
+    let resultArray = [];
+
+    let currentKeyIndex = 0;
+    for(let i = 0; i < textArray.length; i++){
+        if(currentKeyIndex == keyArray.length){
+            currentKeyIndex = 0;
+        }
+        let currentLetterIndex = letters.indexOf(textArray[i]);
+        let keyIndex = letters.indexOf(keyArray[currentKeyIndex]);
+        let newLetterIndex = (currentLetterIndex + letters.length + keyIndex) % letters.length;
+        resultArray.push(letters[newLetterIndex]);
+        currentKeyIndex++;
+    }
+
+    return resultArray;
+}
+
+console.log(DecryptVigenere('aelkqgvpppdw', 'password'));
+console.log(EncryptVigenere('letsusemaple', 'password'));
